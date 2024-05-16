b121e0eac5b6cb2716e8338f918f6ffba6ee1a2c
Chris Guizzetti <chrisguizzetti@chriss-mbp.lan>
2024-05-16 09:48:25 -0700

    styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,24 @@ import "./index.css";
 import BlogCard from "./BlogCard";
 import image from "./assets/spacejoy-YqFz7UMm8qE-unsplash.jpg";
 
-const Card = [
-  {
-    img: image,
-    label: "Interior",
-    title: "Top 5 Living Room Inspirations",
-    message:
-      "Curated vibrant colors for your living, make it pop & calm at the same time.",
-    link: "https://www.google.com",
-  },
-];
+const Card = {
+  img: image,
+  label: "Interior",
+  title: "Top 5 Living Room Inspirations",
+  message:
+    "Curated vibrant colors for your living, make it pop & calm at the same time.",
+  link: "https://www.google.com",
+};
 
 function App() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gradient-to-br from-gray-light to-gray-medium p-4">
+    <div className="flex flex-col items-center min-h-screen bg-gradient-to-br from-gray-light to-gray-medium p-4 sm:bg-gradient-to-r sm:from-gray-light sm:to-gray-medium">
       <BlogCard
-        img={Card[0].img}
-        label={Card[0].label}
-        title={Card[0].title}
-        message={Card[0].message}
-        link={Card[0].link}
+        img={Card.img}
+        label={Card.label}
+        title={Card.title}
+        message={Card.message}
+        link={Card.link}
       />
     </div>
   );
--- src/BlogCard.tsx
+++ src/BlogCard.tsx
@@ -16,11 +16,15 @@ const BlogCard: React.FC<BlogCardProps> = ({
   link,
 }) => {
   return (
-    <div className="max-w-sm m-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-sm">
+    //     <div className="mt-0 sm:mt-20 md:mt-40">
+    // </div>
+    <div className="max-w-sm mt-20 md:m-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-sm">
       <img className="h-80 w-full object-cover" src={img} alt="article image" />
       <div className="p-4">
         <div className="label mb-4">{label}</div>
-        <h3 className="text-neutral-900 text-xl font-bold mb-2">{title}</h3>
+        <h3 className="text-neutral-900 text-lg md:text-xl font-bold mb-2">
+          {title}
+        </h3>
         <p className="text-neutral-600 text-base mb-4">{message}</p>
         <a href={link} className={link ? "btn-normal" : "btn-disabled"}>
           Read more {"->"}
